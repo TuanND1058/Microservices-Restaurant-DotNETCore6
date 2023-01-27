6569c177d87a8fc352f78ead944391c497e538c9
TuanND <tuannd1058@gmail.com>
2023-01-27 10:16:45 +0700

    Product repository interface and DTOS

---
+++ Restaurant.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Restaurant.Web.Models;
+
+namespace Restaurant.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(Guid id);
+        Task<T> CreateProductAsync<T>(ProductDto product);
+        Task<T> UpdateProductAsync<T>(ProductDto product);
+        Task<T> DeleteProductAsync<T>(Guid id);
+    }
+}
