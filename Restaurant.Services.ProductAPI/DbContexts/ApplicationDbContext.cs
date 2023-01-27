53abb37fa1e750db42cc15d6f2242a20dbcaf0aa
TuanND <tuannd1058@gmail.com>
2023-01-27 09:50:39 +0700

    Seed products in ProductAPI

--- Restaurant.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Restaurant.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -11,5 +11,47 @@ namespace Restaurant.Services.ProductAPI.DbContexts
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = Guid.NewGuid(),
+                Name = "Samosa",
+                Price = 15,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = Guid.NewGuid(),
+                Name = "Paneer Tikka",
+                Price = 13.99,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = Guid.NewGuid(),
+                Name = "Sweet Pie",
+                Price = 10.99,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "",
+                CategoryName = "Dessert"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = Guid.NewGuid(),
+                Name = "Pav Bhaji",
+                Price = 15,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "",
+                CategoryName = "Entree"
+            });
+        }
     }
 }
