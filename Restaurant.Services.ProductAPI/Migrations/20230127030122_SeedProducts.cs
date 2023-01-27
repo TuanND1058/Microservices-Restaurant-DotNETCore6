ff9f987515bffa63d016c898f870d45933d4191e
TuanND <tuannd1058@gmail.com>
2023-01-27 10:04:55 +0700

    images

---
+++ Restaurant.Services.ProductAPI/Migrations/20230127030122_SeedProducts.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant.Services.ProductAPI.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { new Guid("3bd2b860-4546-4da5-b4e0-4bf71786e80a"), "Appetizer", "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://dotnetmastery.blob.core.windows.net/mango/11.jpg", "Samosa", 15.0 },
+                    { new Guid("64c1e17b-a4f6-4c36-94d2-ae7869f1a7c5"), "Appetizer", "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://dotnetmastery.blob.core.windows.net/mango/12.jpg", "Paneer Tikka", 13.99 },
+                    { new Guid("67ff1dcb-95f4-41e4-a320-5b3894abfbe2"), "Dessert", "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://dotnetmastery.blob.core.windows.net/mango/13.jpg", "Sweet Pie", 10.99 },
+                    { new Guid("9291de74-950c-4e88-b482-9499062854b3"), "Entree", "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://dotnetmastery.blob.core.windows.net/mango/14.jpg", "Pav Bhaji", 15.0 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("3bd2b860-4546-4da5-b4e0-4bf71786e80a"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("64c1e17b-a4f6-4c36-94d2-ae7869f1a7c5"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("67ff1dcb-95f4-41e4-a320-5b3894abfbe2"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("9291de74-950c-4e88-b482-9499062854b3"));
+        }
+    }
+}
