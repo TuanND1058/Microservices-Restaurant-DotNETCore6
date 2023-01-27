72f06e7457e6752d2e4cb1a79e846b7501e7c316
TuanND <tuannd1058@gmail.com>
2023-01-27 15:32:21 +0700

    Product Controller Get All Action

---
+++ Restaurant.Web/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Restaurant.Web.Models;
+using Restaurant.Web.Services.IServices;
+
+namespace Restaurant.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
