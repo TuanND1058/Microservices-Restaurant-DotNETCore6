5aadd5e6c0ad79efa1aff180267bfb709bc82db5
TuanND <tuannd1058@gmail.com>
2023-02-02 14:46:06 +0700

    Passing custom claims

--- Restaurant.Web/Controllers/HomeController.cs
+++ Restaurant.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Restaurant.Web.Models;
 using System.Diagnostics;
@@ -31,8 +32,10 @@ namespace Restaurant.Web.Controllers
         }
 
         [Authorize]
-        public IActionResult Login()
+        public async Task<IActionResult> Login()
         {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
             return RedirectToAction(nameof(Index));
         }
 
