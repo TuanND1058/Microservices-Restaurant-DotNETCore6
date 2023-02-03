3782d0d2a7f000c770d63ba0b00c3e10fd7f9f7a
TuanND <tuannd1058@gmail.com>
2023-02-03 14:23:56 +0700

    Cart Repository Interface

---
+++ Restaurant.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -0,0 +1,27 @@
+﻿using Restaurant.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Restaurant.Services.ShoppingCartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        public Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDto> GetCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(Guid cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
