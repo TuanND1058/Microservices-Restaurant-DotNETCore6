a037bbf2a096b1ed894d211d2d2535018b88c2a4
TuanND <tuannd1058@gmail.com>
2023-02-03 14:01:08 +0700

    Nuget, Class file for shopping cart

---
+++ Restaurant.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Restaurant.Services.ShoppingCartAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Restaurant.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace Restaurant.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration ReisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //TODO
+            });
+
+            return mappingConfig;
+        }
+    }
+}
