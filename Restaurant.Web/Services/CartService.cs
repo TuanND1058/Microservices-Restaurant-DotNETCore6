edee7e292b29e9d7f22f881f15fa6f3c89a0ca9e
TuanND <tuannd1058@gmail.com>
2023-02-06 15:25:46 +0700

    Define and configure Cart Service

---
+++ Restaurant.Web/Services/CartService.cs
@@ -0,0 +1,58 @@
+﻿using Restaurant.Web.Models;
+using Restaurant.Web.Services.IServices;
+
+namespace Restaurant.Web.Services
+{
+    public class CartService : BaseService, ICartService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CartService(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<T> AddToCartAsync<T>(CartDto cartDto, string token = null)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/AddCart",
+                AccessToken = token
+            });
+        }
+
+        public async Task<T> GetCartByUserIdAsync<T>(string userId, string token = null)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/GetCart" + userId,
+                AccessToken = token
+            });
+        }
+
+        public async Task<T> RemoveToFromCartAsync<T>(Guid cartId, string token = null)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartId,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/RemoveCart",
+                AccessToken = token
+            });
+        }
+
+        public async Task<T> UpdateCartAsync<T>(CartDto cartDto, string token = null)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/UpdateCart",
+                AccessToken = token
+            });
+        }
+    }
+}
---
+++ Restaurant.Web/Services/IServices/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using Restaurant.Web.Models;
+
+namespace Restaurant.Web.Services.IServices
+{
+    public interface ICartService
+    {
+        Task<T> GetCartByUserIdAsync<T>(string userId, string token = null);
+        Task<T> AddToCartAsync<T>(CartDto cartDto, string token = null);
+        Task<T> UpdateCartAsync<T>(CartDto cartDto, string token = null);
+        Task<T> RemoveToFromCartAsync<T>(Guid cartId, string token = null);
+    }
+}
