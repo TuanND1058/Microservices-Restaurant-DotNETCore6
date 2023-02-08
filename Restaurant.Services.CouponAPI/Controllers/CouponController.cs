666f1cbf8921ae31e1f8c8d76138aae95aec6fca
TuanND <tuannd1058@gmail.com>
2023-02-08 09:01:27 +0700

    Coupon Controller

---
+++ Restaurant.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Services.CouponAPI.Models.Dto;
+using Restaurant.Services.CouponAPI.Repository;
+
+namespace Restaurant.Services.CouponAPI.Controllers
+{
+    [ApiController]
+    [Route("api/coupon")]
+    public class CouponController : Controller
+    {
+        private readonly ICouponRepository _couponRepository;
+        protected ResponseDto _response;
+
+        public CouponController(ICouponRepository cartRepository)
+        {
+            _couponRepository = cartRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet("{code}")]
+        public async Task<object> GetDiscountForCode(string code)
+        {
+            try
+            {
+                var coupon = await _couponRepository.GetCouponByCode(code);
+                _response.Result = coupon;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
