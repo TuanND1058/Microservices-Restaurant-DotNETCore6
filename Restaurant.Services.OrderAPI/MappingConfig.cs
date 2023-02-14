82675cfbb12b0c06fbd105345d8420b05de182c3
TuanND <tuannd1058@gmail.com>
2023-02-14 09:29:09 +0700

    Startup file and Migrations

---
+++ Restaurant.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace Restaurant.Services.OrderAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration ReisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+
+            });
+
+            return mappingConfig;
+        }
+    }
+}
