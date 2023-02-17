d3146c2d0ccd246b5beeeee69e1fe1eba48b468c
TuanND <tuannd1058@gmail.com>
2023-02-17 16:50:58 +0700

    PaymentAPI

---
+++ Restaurant.Services.PaymentAPI/Messages/UpdatePaymentResultMessage.cs
@@ -0,0 +1,10 @@
+﻿using Restaurant.MessageBus;
+
+namespace Restaurant.Services.PaymentAPI.Messages
+{
+    public class UpdatePaymentResultMessage : BaseMessage
+    {
+        public Guid OrderId { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Restaurant.Services.PaymentAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,84 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using PaymentProcessor;
+using Restaurant.MessageBus;
+using Restaurant.Services.PaymentAPI.Messages;
+using System.Text;
+
+namespace Restaurant.Services.PaymentAPI.Messaging
+{
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
+    {
+        private readonly string serviceBusConnectionString;
+        private readonly string subscriptionPayment;
+        private readonly string orderPaymentProcessTopic;
+        private readonly string orderUpdatePaymentResultTopic;
+
+        private readonly ServiceBusProcessor orderPaymentProcessor;
+
+        private readonly IProcessPayment _processPayment;
+        private readonly IConfiguration _configuration;
+        private readonly IMessageBus _messageBus;
+
+        public AzureServiceBusConsumer(IConfiguration configuration, IMessageBus messageBus, IProcessPayment processPayment)
+        {
+            _configuration = configuration;
+            _messageBus = messageBus;
+            _processPayment = processPayment;
+
+            serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
+            subscriptionPayment = _configuration.GetValue<string>("OrderPaymentProcessSubscription");
+            orderUpdatePaymentResultTopic = _configuration.GetValue<string>("OrderUpdatePaymentResultTopic");
+            orderPaymentProcessTopic = _configuration.GetValue<string>("OrderPaymentProcessTopics");
+
+            var client = new ServiceBusClient(serviceBusConnectionString);
+
+            orderPaymentProcessor = client.CreateProcessor(orderPaymentProcessTopic, subscriptionPayment);
+        }
+
+        public async Task Start()
+        {
+            orderPaymentProcessor.ProcessMessageAsync += ProcessPayments;
+            orderPaymentProcessor.ProcessErrorAsync += ErrorHandler;
+            await orderPaymentProcessor.StartProcessingAsync();
+        }
+
+        public async Task Stop()
+        {
+            await orderPaymentProcessor.StopProcessingAsync();
+            await orderPaymentProcessor.DisposeAsync();
+        }
+
+        Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task ProcessPayments(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            PaymentRequestMessage paymentRequestMessage = JsonConvert.DeserializeObject<PaymentRequestMessage>(body);
+
+            var result = _processPayment.PaymentProcessor();
+
+            UpdatePaymentResultMessage updatePaymentResultMessage = new()
+            {
+                Status = result,
+                OrderId = paymentRequestMessage.OrderId
+            };
+
+            try
+            {
+                await _messageBus.PublishMessage(updatePaymentResultMessage, orderPaymentProcessTopic);
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
